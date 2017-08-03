1ceef35d99a363f6b92c7bb904cb50ad5d173585
Felix S <xLifeHD@gmail.com>
2017-08-03 16:10:27 +0200

    Initial commit

---
+++ src/main/java/com/gmail/xlifehd/xrandomtp/Main.java
@@ -0,0 +1,47 @@
+package com.gmail.xlifehd.xrandomtp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.gmail.xlifehd.xrandomtp.commands.RandomTP;
+
+public class Main extends JavaPlugin {
+	
+	private static Main instance;
+	
+	public static Main getPlugin() {
+		return instance;
+	}
+	
+	public static String pluginPrefix = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "xRandom" + ChatColor.GREEN + ChatColor.BOLD + "TP" + ChatColor.RESET + ChatColor.DARK_GRAY + "] ";
+	public static String errorPrefix = pluginPrefix + ChatColor.RED;
+	public static String infoPrefix = pluginPrefix + ChatColor.WHITE;
+	
+	@Override
+	public void onEnable() {
+		
+		instance = this;
+		
+		//Configuration
+		FileConfiguration config = this.getConfig();
+		
+		config.options().header("xRandomTP Config by xLifeHD@gmail.com");
+		config.addDefault("CfgVersion", 1);
+		config.addDefault("border.maxRadius", 5000);
+		config.addDefault("border.minRadius", 200);
+		config.addDefault("teleport.maxTries", 10);
+		config.options().copyHeader(true);
+		config.options().copyDefaults(true);
+		saveConfig();
+		
+		//Command register
+		this.getCommand("rtp").setExecutor(new RandomTP());
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
---
+++ src/main/java/com/gmail/xlifehd/xrandomtp/commands/RandomTP.java
@@ -0,0 +1,201 @@
+package com.gmail.xlifehd.xrandomtp.commands;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import com.gmail.xlifehd.xrandomtp.Main;
+
+public class RandomTP implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		//Checking if it's a player and if the player has the right permissions
+		if (sender instanceof Player && ( sender.hasPermission( "xrandomtp.rtp" ) || sender.isOp() ) ) {
+			
+			Player player = (Player) sender;
+			
+			if ( args.length >= 1 ) {
+				//Print help
+				if ( args[0].equalsIgnoreCase( "help" ) ) {
+					player.chat( "/help rtp" );
+				}
+				
+			} else {
+				//Teleport
+				randomTeleport( player );
+				
+			}
+			
+			return true;
+			
+		} else if ( sender instanceof Player ) {
+			
+			sender.sendMessage(Main.errorPrefix + "You are lacking the permission \"xrandomtp.rtp\"!");
+			return true;
+			
+		} else {
+			
+			sender.sendMessage(Main.errorPrefix + "You have to be a player!");
+			return true;
+			
+		}
+		
+	}
+	
+	private void randomTeleport ( Player player ) {
+		//Loading the config
+		FileConfiguration config = Main.getPlugin().getConfig();
+		int maxRadius = config.getInt( "border.maxRadius" );
+		int minRadius = config.getInt( "border.minRadius" );
+		int tries = config.getInt("teleport.maxTries");
+		
+		//Checking config
+		if ( maxRadius < 0 ) {
+			maxRadius = 0;
+			Main.getPlugin().getLogger().warning( Main.errorPrefix + "Max. radius too small! ERROR#100" );
+		}
+		
+		if ( minRadius < 0 ) {
+			minRadius = 0;
+			Main.getPlugin().getLogger().warning( Main.errorPrefix + "Min. radius too small! ERROR#101" );
+		}
+		
+		if ( minRadius >= maxRadius ) {
+			maxRadius = minRadius + 1;
+			Main.getPlugin().getLogger().warning( Main.errorPrefix + "Max. radius has to be bigger than min. radius! ERROR#102" );
+		}
+		
+		if ( tries < 5 ) {
+			tries = 5;
+			Main.getPlugin().getLogger().warning( Main.errorPrefix + "Min. tries are 5! ERROR#103" );
+		}
+		
+		if ( tries > 50 ) {
+			tries = 50;
+			Main.getPlugin().getLogger().warning( Main.errorPrefix + "Max. tries are 50! ERROR#104" );
+		}
+		
+		//Initilizing variables
+		Location randomLoc = player.getLocation();
+		boolean safe = false;
+		
+		//Try getting a safe location 5 times, if it fails print a message
+		for ( int i = 0; i < tries && !safe; i++ ) {
+			
+			//Getting random polar coordinates
+			double distance = minRadius + ( Math.random() * ( maxRadius - minRadius + 1 ) );
+			double angle = Math.random() * 2 * Math.PI;
+			
+			//Calculating cartesian coordinates
+			int xCoordinate = (int) ( Math.cos( angle ) * distance );
+			int zCoordinate = (int) ( Math.sin( angle ) * distance );
+			
+			//Getting the y-Coordinate
+			int yCoordinate = player.getWorld().getHighestBlockYAt( xCoordinate, zCoordinate );
+			
+			//Creating new Location
+			randomLoc = new Location( player.getWorld(), xCoordinate + 0.5, yCoordinate, zCoordinate + 0.5 );
+			
+			//Checking if it's safe
+			safe = isSafe( randomLoc );
+		}
+		
+		if ( safe ) {
+			//Teleporting the player
+			player.teleport( randomLoc );
+			
+			//Informing the player
+			player.sendMessage(Main.infoPrefix + "You have been teleported randomly!");
+			
+		} else {
+			player.sendMessage(Main.infoPrefix + "Couldn't find a safe spot! Please try again!");
+		}
+	}
+	
+	private boolean isSafe( Location loc ) {
+		
+		int xCoordinate = loc.getBlockX();
+		int zCoordinate = loc.getBlockZ();
+		int yCoordinate = loc.getWorld().getHighestBlockYAt( xCoordinate, zCoordinate );
+		
+		for ( short i = -1; i <= 0; i++ ) {
+			
+			Block block = loc.getWorld().getBlockAt( xCoordinate, yCoordinate + i, zCoordinate );
+			Material material = block.getType();
+			
+			switch( material ) {
+			case ENDER_PORTAL:
+			case LAVA:
+			case PORTAL:
+			case STATIONARY_LAVA:
+			case STATIONARY_WATER:
+			case FIRE:
+			case MAGMA:
+			case CACTUS:
+				return false;
+				
+			default:
+				break;
+			}
+			
+			switch ( i ) {
+			case -1:
+				
+				//Check if block is solid and not liquid
+				if ( !( !block.isLiquid() && material.isSolid() ) ) {
+					return false;
+				}
+				break;
+				
+			case 0:
+				
+				//Check if block is not liquid / solid
+				if ( !( !block.isLiquid() && !material.isSolid() ) ) {
+					return false;
+				}
+				break;
+				
+			default:
+				
+				//Throw Error
+				Main.getPlugin().getLogger().warning( Main.errorPrefix + "Something went wrong! ERROR#0" );
+				return false;
+				
+			}
+			
+		}
+		
+		return true;
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
